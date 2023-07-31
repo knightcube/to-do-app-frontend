ad84fc3d49e8534214964c284be67cd497b78ae7
Rajat Kumar Gupta <rajatcube@gmail.com>
2023-07-31 11:29:56 +0530

    Implemented Authentication via useParams

--- README.md
+++ README.md
@@ -8,11 +8,16 @@
     * State
     * Props
 * Bootstrap
-* Routing
+* Routing 
+    * Hooks - useNavigate, useParams
+    * Default Routing 
+    * Handled Routing for wrong urls such as http://localhost:3000/abcdefgh
 * Forms
 * Validation
 * REST API Calls   
 * Authentication (Basic and JWT Tokens) 
 
+## What third party npm modules did I use?
+* react-router-dom
 <!-- ## What did I learn in Spring Boot? -->
 <!-- *  -->
--- src/components/todo/ToDoApp.js
+++ src/components/todo/ToDoApp.js
@@ -1,6 +1,6 @@
 import "./ToDoApp.css";
 import { useState } from "react";
-import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useNavigate, useParams } from "react-router-dom";
 
 function ToDoApp(){
     return (
@@ -9,7 +9,8 @@ function ToDoApp(){
                 <Routes>
                     <Route path='/' element={<LoginComponent/>}></Route>
                     <Route path = '/login' element={<LoginComponent/>}></Route>
-                    <Route path = '/welcome' element={<WelcomeComponent/>}></Route>
+                    <Route path = '/welcome/:username' element={<WelcomeComponent/>}></Route>
+                    <Route path = '/*' element={<ErrorComponent/>}></Route>
                 </Routes>
             </BrowserRouter>
         </div>
@@ -41,7 +42,7 @@ function LoginComponent(){
             console.log("success")
             setShowSuccessMessage(true)
             setShowErrorMessage(false)
-            navigate('/welcome')
+            navigate(`/welcome/${username}`)
         }else{
             console.log("failed")
             setShowSuccessMessage(false)
@@ -96,9 +97,26 @@ function LoginComponent(){
 }
 
 function WelcomeComponent(){
+    
+    const {username} = useParams()
+
+    console.log(username)
+
     return (
-        <div>
-            Welcome Component
+        <div className="WelcomeComponent">
+            <h1>Welcome {username}</h1>
+            <div>
+                Welcome Component
+            </div>
+        </div>
+    )
+}
+
+function ErrorComponent(){
+    return (
+        <div className="ErrorComponent">
+            <h1>We are working really hard!</h1>
+            <div>Apologies for the 404. Reach out to our team at ABC</div>
         </div>
     )
 }
