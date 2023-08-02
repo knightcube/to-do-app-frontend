a3ee0c51c3eb535778211ee3068e6ef25a9a3878
Rajat Kumar Gupta <rajatcube@gmail.com>
2023-08-02 06:32:41 +0530

    Refactored Code Into Individual Components

---
+++ src/components/todo/LoginComponent.js
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function LoginComponent() {
+
+    const [username, setUsername] = useState("admin")
+
+    const [password, setPassword] = useState("")
+
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false)
+
+    const [showErrorMessage, setShowErrorMessage] = useState(false)
+
+    const navigate = useNavigate()
+
+    function handleUsernameChange(event) {
+        setUsername(event.target.value)
+    }
+
+    function handlePasswordChange(event) {
+        setPassword(event.target.value)
+    }
+
+    function handleLoginBtn() {
+        if (username === "knightcube" && password === "pass1234") {
+            console.log("success")
+            setShowSuccessMessage(true)
+            setShowErrorMessage(false)
+            navigate(`/welcome/${username}`)
+        } else {
+            console.log("failed")
+            setShowSuccessMessage(false)
+            setShowErrorMessage(true)
+        }
+    }
+
+    return (
+        <div className="Login">
+            <div className="LoginForm">
+
+                <div>
+                    <label>Username</label>
+                    <input type="text" name="username" value={username} onChange={handleUsernameChange} />
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input type="password" name="password" value={password} onChange={handlePasswordChange} />
+                </div>
+
+                <button type="button" name="login" onClick={handleLoginBtn}>Login  </button>
+
+                {showSuccessMessage && <div>Authenticated Successfully</div>}
+                {showErrorMessage && <div>Authentication Failed. Try again. </div>}
+            </div>
+        </div>
+    )
+}
+
+export default LoginComponent;
